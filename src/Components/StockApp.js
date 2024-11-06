6c983e58538803dd3ccac4ef99ce0674ec8abfff
ellisrugazoora <ellisrgrz@gmail.com>
2024-11-06 14:40:40 +0300

    Update StockApp.js

--- src/Components/StockApp.js
+++ src/Components/StockApp.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function StockApp() {
+  const [input, setInput] = useState('');
+  const [response, setResponse] = useState('');
+  function sendMessage(e){
+    console.log(e.target)
+    let res = "Response to: " + input;
+    setResponse(res)
+    console.log("Send message")
+  }
   return (
     <div className="StockApp">
       <header className="StockApp-header">
@@ -9,6 +17,15 @@ function StockApp() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+        <div>
+          <p>{response}</p>
+        </div>
         <a
           className="StockApp-link"
           href="https://reactjs.org"
